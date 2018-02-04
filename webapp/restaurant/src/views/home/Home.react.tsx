c6ec3dd5920730a0204c0759b97c867b83ace3de
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-02-04 20:37:14 +0000

    Added baseapp for restaurant portal

---
+++ webapp/restaurant/src/views/home/Home.react.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export class Home extends React.Component<any, any> {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                Hello there
+            </div>
+        )
+    }
+}
